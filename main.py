9048190c2b01431ea21a681ba1c98405c8d55a1a
elh1 <elh1@iastate.edu>
2023-02-17 14:57:02 -0600

    update story

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import tkinter as Tk
 
 def main():
-    color = input("Enter a color: ")
-    pluralNoun = input("Enter a plural noun: ")
-    celebrity = input("Enter a celebrity: ")
-    print("Roses are", color)
-    print(pluralNoun + " are blue")
-    print("I love", celebrity)
+    verbing = input("Enter a verb that ends in ing: ")
+    number = input("Enter a number: ")
+    noun1 = input("Enter a noun: ")
+    noun2 = input("Enter a another noun: ")
+    adjective = input("Enter an adjective: ")
+    kid_name = input("Enter name: ")
+    age = input("Enter an age: ")
+
+    # text = f"Roses are {color} \n {plural_noun} are blue \n I love {celebrity}"
+    story = f"Computers are amazing pieces of technology that are really great at {verbing}.\n You would be suprised by the amount of computers you see a day! \n " \
+            f"In an average day, you see {number} computers. \n Computers can be as big as a {noun1} or as small as a {noun2}.\n  Did you know that {adjective} calculators " \
+            f"are computers too?\n  Technology can do so much! \n I wonder what technology will do {kid_name} is {age}!!"
+
+
     label = Tk.Label(
                         None,
-                        text='The text you want to display',
-                        font=('Times', '18'),
+                        text=story,
+                        font=('Times', '24'),
                         fg='blue'
                      )
     label.pack()
